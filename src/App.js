1b80c3b301f53cf3d3c60d94f01c962c0d258ce2
rootMa99 <anasszeroual09@gmail.com>
2024-03-15 22:06:21 +0000

    bn

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Qletter from './component/Qletter';
+import Stable from './component/Stable';
+import Ttable from './component/Ttable';
+
+function App() {
+  return (
+    <div className="App">
+        <Qletter />
+        <Stable />
+        <Ttable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Stable.js
@@ -0,0 +1,84 @@
+import c from "./Stable.module.css";
+
+const Stable = (p) => {
+  return (
+    <table className={c["letter-s"]}>
+      <tr>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+      </tr>
+      <tr>
+        <td className={c.corner}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.corner}></td>
+      </tr>
+      <tr>
+        <td className={c.corner}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.corner}></td>
+      </tr>
+      <tr>
+        <td className={c.corner}></td>
+      </tr>
+      <tr>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+      </tr>
+      <tr>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.corner}></td>
+      </tr>
+      <tr>
+        <td className={c.corner}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.corner}></td>
+      </tr>
+      <tr>
+        <td className={c.corner}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.top}></td>
+        <td className={c.corner}></td>
+      </tr>
+      <tr>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+        <td className={c.corner}></td>
+      </tr>
+    </table>
+  );
+};
+export default Stable;
