749aa542efb89fedb7de4b5b716c63d1861dd870
anass zeroual <anasszeroual09@gmail.com>
2024-03-18 15:28:39 +0000

    topm

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -10,19 +10,19 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import c from "./Home.module.css";
 import React, { useRef, useState } from "react";
-import { getMonthAndYear, getStartAndEndMonth } from "../functions/utils";
+import { getStartAndEndMonth } from "../functions/utils";
 import { useDispatch } from "react-redux";
 import { dataActions } from "../store/dataSlice";
 
 const Home = (p) => {
   const [show, setShow] = useState({ show: false, title: "" });
-  const [selectedDate, setSelectedDate] = useState(getMonthAndYear());
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const scrollRef = useRef(null);
   const dispatch = useDispatch();
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
-    dispatch(dataActions.setTime(getStartAndEndMonth(date)))
+    dispatch(dataActions.setTime(getStartAndEndMonth(date)));
   };
   const clickHandler = (e, t) => {
     setShow({ show: false, title: "" });
@@ -30,11 +30,11 @@ const Home = (p) => {
     scrollRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
-console.log(selectedDate);
+  console.log(selectedDate);
   return (
     <React.Fragment>
       <div className={c.datePicker}>
-      <label>PICK A DATE</label>
+        <label>PICK A DATE</label>
         <DatePicker
           selected={selectedDate}
           onChange={handleDateChange}
@@ -45,70 +45,85 @@ console.log(selectedDate);
         />
       </div>
       <div className={c.head}>
-        <div className={c.wrapAl} onClick={(e) => clickHandler(e, "safety")}>
-          <div className={c.title}>
-            <div className={c.line}></div>
-            <h3>Safety</h3>
+        <div className={c.ndiv} style={{"border":"none"}}>
+          <h1>people</h1>
+          <div className={c.wrapAl} onClick={(e) => clickHandler(e, "safety")}>
+            <div className={c.title}>
+              <div className={c.line}></div>
+              <h3>Safety</h3>
+            </div>
+            <div className={c.alphabetH}>
+              <Ttable date={selectedDate} />
+            </div>
           </div>
-          <div className={c.alphabetH}>
-            <Ttable date={selectedDate}/>
-          </div>
-        </div>
-        <div className={c.wrapAl} onClick={(e) => clickHandler(e, "skills")}>
-          <div className={c.title}>
-            <div className={c.line}></div>
-            <h3>Skills</h3>
-          </div>
-          <div className={c.alphabetH}>
-            <Stable date={selectedDate}/>
-          </div>
-        </div>
-        <div className={c.wrapAl} onClick={(e) => clickHandler(e, "quality")}>
-          <div className={c.title}>
-            <div className={c.line}></div>
-            <h3>quality</h3>
-          </div>
-          <div className={c.alphabetH}>
-            <Qletter date={selectedDate}/>
+          <div className={c.wrapAl} onClick={(e) => clickHandler(e, "skills")}>
+            <div className={c.title}>
+              <div className={c.line}></div>
+              <h3>Skills</h3>
+            </div>
+            <div className={c.alphabetH}>
+              <Stable date={selectedDate} />
+            </div>
           </div>
         </div>
-        <div className={c.wrapAl} onClick={(e) => clickHandler(e, "delivery")}>
-          <div className={c.title}>
-            <div className={c.line}></div>
-            <h3>delivery</h3>
+        <div className={c.ndiv}>
+        <h1>performance</h1>
+          <div className={c.wrapAl} onClick={(e) => clickHandler(e, "quality")}>
+            <div className={c.title}>
+              <div className={c.line}></div>
+              <h3>quality</h3>
+            </div>
+            <div className={c.alphabetH}>
+              <Qletter date={selectedDate} />
+            </div>
           </div>
-          <div className={c.alphabetH}>
-            <Dtable date={selectedDate}/>
+          <div
+            className={c.wrapAl}
+            onClick={(e) => clickHandler(e, "delivery")}
+          >
+            <div className={c.title}>
+              <div className={c.line}></div>
+              <h3>delivery</h3>
+            </div>
+            <div className={c.alphabetH}>
+              <Dtable date={selectedDate} />
+            </div>
+          </div>
+          <div
+            className={c.wrapAl}
+            onClick={(e) => clickHandler(e, "inventory")}
+          >
+            <div className={c.title}>
+              <div className={c.line}></div>
+              <h3>inventory</h3>
+            </div>
+            <div className={c.alphabetH}>
+              <Itable date={selectedDate} />
+            </div>
+          </div>
+          <div
+            className={c.wrapAl}
+            onClick={(e) => clickHandler(e, "productivity")}
+          >
+            <div className={c.title}>
+              <div className={c.line}></div>
+              <h3>productivity</h3>
+            </div>
+            <div className={c.alphabetH}>
+              <Ptable date={selectedDate} />
+            </div>
           </div>
         </div>
-        <div className={c.wrapAl} onClick={(e) => clickHandler(e, "inventory")}>
-          <div className={c.title}>
-            <div className={c.line}></div>
-            <h3>inventory</h3>
-          </div>
-          <div className={c.alphabetH}>
-            <Itable date={selectedDate}/>
-          </div>
-        </div>
-        <div
-          className={c.wrapAl}
-          onClick={(e) => clickHandler(e, "productivity")}
-        >
-          <div className={c.title}>
-            <div className={c.line}></div>
-            <h3>productivity</h3>
-          </div>
-          <div className={c.alphabetH}>
-            <Ptable date={selectedDate}/>
-          </div>
-        </div>
-        <div className={c.wrapAl} onClick={(e) => clickHandler(e, "kaizen")}>
-          <div className={c.title}>
-            <div className={c.line}></div>
-            <h3>kaizen</h3>
-          </div>
-          <div className={c.alphabetH}>
-            <Ktable date={selectedDate}/>
+        <div className={c.ndiv}>
+        <h1>improvement</h1>
+          <div className={c.wrapAl} onClick={(e) => clickHandler(e, "kaizen")}>
+            <div className={c.title}>
+              <div className={c.line}></div>
+              <h3>kaizen</h3>
+            </div>
+            <div className={c.alphabetH}>
+              <Ktable date={selectedDate} />
+            </div>
           </div>
         </div>
       </div>
