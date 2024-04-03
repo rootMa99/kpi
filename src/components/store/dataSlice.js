b857c24abeb271f127c8a07fe4160428d9f67b53
anass zeroual <anasszeroual09@gmail.com>
2024-04-03 09:43:06 +0000

    8965

--- src/components/store/dataSlice.js
+++ src/components/store/dataSlice.js
@@ -1,26 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getCurrentMonthDates, getStartAndEndMonth } from "../functions/utils";
-
+import { getCurrentMonthDates } from "../functions/utils";
 
 const dataSlice = createSlice({
-    name:"datas",
-    initialState:{
-        data:[],
-        kpiOwners:[],
-        date:getCurrentMonthDates()
+  name: "datas",
+  initialState: {
+    data: [],
+    kpiOwners: [],
+    date: getCurrentMonthDates(),
+  },
+  reducers: {
+    addData(s, p) {
+      s.data = p.payload;
     },
-    reducers:{
-        addData(s,p){
-            s.data=p.payload;
-        },
-        setTime(s, p){
-            s.date=p.payload;
-        },
-        setKpiOwners(s,p){
-            s.kpiOwners=p.payload;
-        }
-    }
-}) 
+    setTime(s, p) {
+      s.date = p.payload;
+    },
+    setKpiOwners(s, p) {
+      s.kpiOwners = p.payload;
+    },
+  },
+});
 
-export const dataActions=dataSlice.actions;
+export const dataActions = dataSlice.actions;
 export default dataSlice;
